58a1bb93f1df9ee1c46f85164af6a7812fb9a04b
atnoob303 <atnoob303@gmail.com>
2026-03-28 14:45:36 +0700

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ var els=[],sel=null,selGroup=[],tool='sel',idc=0,hist=[],dtool=null,etab='lua';
 var rulerOn=false;
 var tMode=0,TMODES=['Scale','Move','Rotate','All','Warp'],TICONS=['⤢','✥','↻','⊕','⌀'];
 var hierDrag=null;
-var VERSION='Alpha 0.0.6.8';
+var VERSION='Alpha 0.0.6.8/5';
 var distGuideOn=true; // tia đỏ khoảng cách, mặc định bật
 
 var DEFS={
